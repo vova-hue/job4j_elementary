c5382eb0774b7c98d642dbec2b16bba6292c1c67
vova-hue <nol.vladimir@yandex.ru>
2023-05-01 16:29:33 +0300

    4.1. Разбор первой программы. [#159033 #412526]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Vladimir Nol");
+        System.out.println("18.09.1993");
+    }
+}
