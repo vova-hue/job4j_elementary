7023441f1ed9b80a3f0198b58a3f40c2eadb8115
vova-hue <nol.vladimir@yandex.ru>
2023-05-25 20:08:49 +0300

    1. Модульные тесты. [#159435]

---
+++ src/test/java/ru/job4j/converter/ConverterTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.converter;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert200RblThen3dot3333Dlr() {
+        float in = 200;
+        float expected = 3.3333F;
+        float out = Converter.rubleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
