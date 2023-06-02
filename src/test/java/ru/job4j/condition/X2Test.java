cb87aae8bac2c258f6b3439c9f2acd2f9351def9
vova-hue <nol.vladimir@yandex.ru>
2023-06-02 19:55:56 +0300

    1.2. Входные данные в тесте [#297750 #420206]

--- src/test/java/ru/job4j/condition/X2Test.java
+++ src/test/java/ru/job4j/condition/X2Test.java
@@ -39,6 +39,17 @@ public class X2Test {
     }
 
     @Test
+    public void whenA1B1C0X1Then2() {
+        int a = 1;
+        int b = 1;
+        int c = 0;
+        int x = 1;
+        int expected = 2;
+        int rsl = X2.calc(a, b, c, x);
+        Assert.assertEquals(expected, rsl);
+    }
+
+    @Test
     public void whenA1B1C1X0Then1() {
         int a = 1;
         int b = 1;
