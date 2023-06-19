f1d39139ee05867ff87b60b8aced82991ca6c9d4
vova-hue <nol.vladimir@yandex.ru>
2023-06-19 21:24:49 +0300

    4.6.1. Switch [#334070]

---
+++ src/main/java/ru/job4j/condition/SwitchWeek.java
@@ -0,0 +1,31 @@
+package ru.job4j.condition;
+
+public class SwitchWeek {
+    public static String nameOfDay(int day) {
+        String name;
+        switch (day) {
+            case 1 :
+                name = "Понедельник";
+                break;
+            case 2 :
+                name = "Вторник";
+                break;
+            case 3 :
+                name = "Среда";
+                break;
+            case 4 :
+                name = "Четверг";
+                break;
+            case 5 :
+                name = "Пятница";
+                break;
+            case 6 :
+                name = "Суббота";
+                break;
+            default:
+                name = "Воскресенье";
+                break;
+        }
+        return name;
+    }
+}
--- src/main/java/ru/job4j/condition/ThreeMax.java
+++ src/main/java/ru/job4j/condition/ThreeMax.java
@@ -2,15 +2,6 @@ package ru.job4j.condition;
 
 public class ThreeMax {
     public static int max(int first, int second, int third) {
-       // int result;
-       // if (first >= second && first >= third) {
-       //     result = first;
-       // } else if (second >= first && second >= third) {
-         //   result = second;
-       // } else {
-        //    result = third;
-       // }
-       // return result;
         int result = third;
         if (first >= second && first >= third) {
             result = first;
