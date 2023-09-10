fb59b8f1154fb168c42c67a09e32cc48407fb0fc
vova-hue <nol.vladimir@yandex.ru>
2023-09-10 20:24:57 +0300

    5.3. Построить шахматную доску в псевдографике. [#13559 #443745]

--- src/main/java/ru/job4j/loop/Board.java
+++ src/main/java/ru/job4j/loop/Board.java
@@ -4,14 +4,12 @@ public class Board {
     public static void paint(int width, int height) {
         for (int row = 0; row < height; row++) {
                 for (int cell = 0; cell < width; cell++) {
-                    /* печатаем X - width раз. */
                     if ((row + cell) % 2 == 0) {
                         System.out.print("X");
                     } else {
                         System.out.print(" ");
                     }
                 }
-            /* добавляем перевод на новую строку. */
             System.out.println();
         }
     }
