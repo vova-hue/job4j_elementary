55f2d97ee48fcac8b9fd2846e2566ba9ec47323a
vova-hue <nol.vladimir@yandex.ru>
2023-09-14 14:53:53 +0300

    6.0.2. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,7 +3,10 @@ package ru.job4j.array;
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] age = new short[10];
+        System.out.println("Размер массива равен: " + age.length);
         String[] surname = new String[100500];
+        System.out.println("Размер массива равен: " + surname.length);
         float[] prices = new float[40];
+        System.out.println("Размер массива равен: " + prices.length);
     }
 }
