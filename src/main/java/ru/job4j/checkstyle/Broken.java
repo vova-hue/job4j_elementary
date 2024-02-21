c70848b508144236382c36425e00f6e654775bd4
user <nol.vladimir@yandex.ru>
2024-02-21 12:10:50 +0300

    6. Правила Checkstyle [#504981 #460427]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,29 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+    private int sizeOfEmpty = 10;
+
+    public String surname;
+
+    public static final String NEWVALUE = "";
+
+    void echo() { }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+
+    String name;
+
+    Broken() {
+
+    }
+
+}
+
